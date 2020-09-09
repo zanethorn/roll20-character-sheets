32009aae16b4b43cb17ca84830db09a32476acee
Zane Thorn <zane.thorn@lucinahealth.com>
2020-09-09 18:29:55 -0400

    Added HeroQuest sheets to repo

---
+++ HeroQuest/HeroQuest.js
@@ -0,0 +1,18 @@
+class HeroQuest {
+
+
+    checkInstall(){
+
+    }
+
+    register(){
+        
+    }
+}
+
+on("ready", function(){
+    'use strict';
+    var game = new HeroQuest();
+    game.checkInstall();
+    game.register();
+});
